a50d73e84e0c36444b9620b8e9c8bf7f8e88d115
Anthony Chen <anthonyschenemail@gmail.com>
2022-05-08 19:45:58 -0700

    Revert "Revert "Merging to update to most recent app""
    This reverts commit 3c661fac2758c03b9a40d24d3c87dbbf44ea8ea7.

--- app/src/main/java/com/example/group13zoosearch/MainActivity.java
+++ app/src/main/java/com/example/group13zoosearch/MainActivity.java
@@ -1,17 +1,12 @@
 package com.example.group13zoosearch;
 
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 //import androidx.appcompat.widget.SearchView;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.style.ForegroundColorSpan;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
+
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
@@ -25,21 +20,12 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        AnimalList animalList = new AnimalList();
 
-        ListView listView = findViewById(R.id.exhibit_list);
-        adapter
-                = new ArrayAdapter<String>(
-                this,
-                android.R.layout.simple_list_item_1,
-                animalList.selected_exhibits);
-        listView.setAdapter(adapter);
     }
 
 
@@ -48,6 +34,10 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void OnViewExhibitsClicked(View view) {
+        Intent intent = new Intent(this, ViewExhibitsActivity.class);
+        startActivity(intent);
+    }
   //leaving this in here because we will need a directions launch eventually
     public void directionsLaunched(View view) {
         Intent DirectionsIntent = new Intent(this, DirectionsActivity.class);
