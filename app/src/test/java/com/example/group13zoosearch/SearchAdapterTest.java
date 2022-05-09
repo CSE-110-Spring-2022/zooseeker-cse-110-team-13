2f776a5745b6650116596b6e2170adb260719ee7
Anthony Chen <anthonyschenemail@gmail.com>
2022-05-08 20:12:15 -0700

    robelectric

---
+++ app/src/test/java/com/example/group13zoosearch/SearchAdapterTest.java
@@ -0,0 +1,12 @@
+package com.example.group13zoosearch;
+
+import org.junit.Before;
+
+public class SearchAdapterTest {
+    private MainActivity activity;
+    @Before
+    public void setup()
+    {
+        
+    }
+}
